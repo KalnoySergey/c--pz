08c7444f39224bd9a444559f5328c1b21618a094
PolinaCh14 <chernyshovapolina014@gmail.com>
2026-09-18 20:53:17 +0300

    feat: add core code

---
+++ LR1_TestCode1/LR1_TestCode1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using LR1_TestCode1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LR1_TestCode1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
